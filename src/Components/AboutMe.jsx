416352afc058325127132fcf57f79a5b86a5b3e5
Paigomanh <rahulrahulsajeevan007@gmail.com>
2024-12-27 18:24:58 +0530

    slight changes

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -1,18 +1,17 @@
-import React from 'react'
+import React from 'react';
 
 const AboutMe = () => {
   return (
-    <div className='mt-5 lg:mt-0 lg:w-full w-[290px] lg:h-[200px] h-[350px] border border-gray-200 rounded-md'>
-      <div className='text-xl font-bold pt-3 pl-5'>About Me</div>
-      <p className="lg:w-[670px] w-[270px] pl-5 pt-2 text-pretty lg:text-[110%]  text-wrap text-sm text-justify text-gray-500">
+    <div className="mt-5 lg:mt-0 w-[290px] lg:w-full lg:h-auto h-auto border border-gray-200 rounded-md p-5">
+      <h1 className="text-2xl lg:text-3xl font-bold">About Me</h1>
+      <p className="mt-3 w-full lg:w-full w-[280px] text-sm lg:text-base text-justify leading-relaxed text-gray-500">
         I’m a full-stack blockchain developer skilled in React, Next.js, Tailwind CSS, and Solidity. I build decentralized applications (DApps) with a focus on seamless user experiences and robust functionality.
       </p>
-      <p className="lg:w-[670px] w-[270px] pl-5 lg:pt-2 pt-5 font-sans lg:text-[110%] text-wrap text-sm text-pretty text-justify text-gray-500">
+      <p className="mt-3 text-sm lg:w-full w-[250px] lg:text-base text-justify leading-relaxed text-gray-500">
         I work with Ethereum for smart contracts and explore enterprise solutions using Hyperledger Fabric. Passionate about technology, I enjoy creating innovative solutions and tackling challenges in web and blockchain development.
       </p>
-
     </div>
-  )
-}
+  );
+};
 
-export default AboutMe
+export default AboutMe;
